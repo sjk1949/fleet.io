4fb7d1fc64074e4d6172aca47c5db9d7a60ef3ac
Shao Jinkui <shaojinkui21@mails.ucas.ac.cn>
2023-06-07 14:44:39 +0800

    Add graphics zoom function

--- demo.py
+++ demo.py
@@ -3,20 +3,14 @@ from math import *
 from vec import *
 from fleet import *
 
-(width,height) = (1000,600)
-
-def posToScreen(pos):
-    result = Vec()
-    result.x = pos.x+width/2
-    result.y = -pos.y+height/2
-    return result
+(Width,Height) = (1000,600)
 
 class Game:
 
     def __init__(self):
         # pygame setup
         pygame.init()
-        self.screen = pygame.display.set_mode((width,height))
+        self.screen = pygame.display.set_mode((Width,Height))
         self.clock = pygame.time.Clock()
         self.running = True
         #game content
@@ -31,6 +25,12 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                if event.type == pygame.MOUSEWHEEL:
+                    Render.scale += 0.02*event.y
+                    if(Render.scale<Render.minScale):
+                        Render.scale = Render.minScale
+                    print(event.y,Render.scale)
+
             # fill the screen with a color to wipe away anything from last frame
             self.screen.fill("skyblue")
 
@@ -42,7 +42,8 @@ class Game:
 
             # RENDER YOUR GAME HERE
             for render in self.render:
-                self.screen.blit(render.newimage,(render.screenPos.x,render.screenPos.y))
+                render.Render(self.screen)
+                
 
             # flip() the display to put your work on screen
             pygame.display.flip()
@@ -60,47 +61,67 @@ class Player:
 
     def Update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
+        if(keys[pygame.K_w] or keys[pygame.K_UP]):
             self.craft.P = self.craft.Pmax
-            print("up")
         else:
             self.craft.P = 0
-        if keys[pygame.K_LEFT]:
-            self.craft.angle += 0.4
-        if keys[pygame.K_RIGHT]:
-            self.craft.angle -= 0.4
+        if(keys[pygame.K_a] or keys[pygame.K_LEFT]):
+            self.craft.angle += 0.3
+        if(keys[pygame.K_d] or keys[pygame.K_RIGHT]):
+            self.craft.angle -= 0.3
         self.craft.Update()
 
     pass
 
 class Render:
+    minScale = 0.2
+    scale = 1
 
     def __init__(self,object,image):
         self.object = object
         self.pos = self.object.pos
         self.angle = self.object.angle
-        self.image = pygame.image.load(image)
+        self.originImage = pygame.image.load(image)
         #图形相关
-        self.screenPos = posToScreen(self.pos)
-        self.length = 200#界面中图形长度
-        width = self.image.get_height()
-        length = self.image.get_width()
-        self.width = self.length*width/length
-        self.image = pygame.transform.smoothscale(self.image,(self.length,self.width))
+        self.screenPos = Render.posToScreen(self.pos)
+        self.length = 200/Render.scale#界面中图形长度
+        width = self.originImage.get_height()
+        length = self.originImage.get_width()
+        self.ratio = length/width
+        self.image = pygame.transform.smoothscale(self.originImage,(self.length,self.width))
         self.newimage = pygame.transform.rotate(self.image,self.angle)
+        self.rect = self.newimage.get_rect(center=(self.screenPos.x,self.screenPos.y))
+
+    @property
+    def width(self):
+        return self.length/self.ratio
 
     def Update(self):
         #图像更新
         self.pos = self.object.pos
         self.angle = self.object.angle
-        self.screenPos = posToScreen(self.pos)
+        self.length = 200/Render.scale
+        self.screenPos = Render.posToScreen(self.pos)
+        self.image = pygame.transform.smoothscale(self.originImage,(self.length,self.width))
         self.newimage = pygame.transform.rotate(self.image,self.angle)
+        self.rect = self.newimage.get_rect(center=(self.screenPos.x,self.screenPos.y))
+
+    def Render(self,screen):
+        screen.blit(self.newimage,self.rect)
+
+    def posToScreen(pos):
+        result = Vec()
+        result.x = pos.x/Render.scale+Width/2
+        result.y = -pos.y/Render.scale+Height/2
+        return result
+
+    pass
 
 if __name__ == '__main__':
     game = Game()
-    craft = Craft("Test",100000,2000,0,0)
-    craftRender = Render(craft,"./picture/craft1.png")
+    craft = Craft("Test",100000,1000,0,0)
+    craft.render = Render(craft,"./picture/craft1.png")
     player = Player(craft)
     game.players.append(player)
-    game.render.append(craftRender)
+    game.render.append(craft.render)
     game.run()
--- fleet.py
+++ fleet.py
@@ -15,8 +15,11 @@ class Craft:
         self.v = Vec()
         self.a = Vec()
         #动力学参数
+        self.startF = 2000.0#启动力
         self.F = Vec()
         self.f = Vec()
+        #图形参数
+        self.render = None
 
     def __str__(self):
         return 'Craft Object (name: %s,mass: %s,power: %s)' % (self.name,self.m,self.P)
@@ -26,10 +29,10 @@ class Craft:
     def Update(self):
         #动力学计算
         if(self.P!=0):
-            if(self.v.length>=0.1):
+            if(self.F.length*self.v.length>=self.Pmax):
                 force = self.P/self.v.length
             else:
-                force = 1000.0
+                force = self.startF
         else:
             force = 0.0
         self.F = Vec(force*cos(pi*self.angle/180),force*sin(pi*self.angle/180))
@@ -38,6 +41,6 @@ class Craft:
         #运动学计算
         self.v = self.v+self.a
         self.pos = self.pos+self.v
-        print("v %lf %lf a %lf %lf F %lf %lf f %lf %lf" % (self.v.x,self.v.y,self.a.x,self.a.y,self.F.x,self.F.y,self.f.x,self.f.y))
+        #print("v %lf %lf a %lf %lf F %lf %lf f %lf %lf P %lf" % (self.v.x,self.v.y,self.a.x,self.a.y,self.F.x,self.F.y,self.f.x,self.f.y,self.F.length*self.v.length))
 
     pass
